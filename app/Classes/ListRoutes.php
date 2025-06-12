de7283293ccc0ab9445f7d83c4d825f83ce0295e
kmlcsl <muhammadkamilcsl19@gmail.com>
2025-06-12 19:18:02 +0700

    Refactor routes using ListRoutes class

---
+++ app/Classes/ListRoutes.php
@@ -0,0 +1,368 @@
+<?php
+
+namespace App\Classes;
+
+class ListRoutes
+{
+    function getDataAuth($index = null)
+    {
+        $data = [
+            [
+                'title' => 'Public Routes',
+                'item' => [
+                    [
+                        'type' => 'public',
+                        'method' => 'get',
+                        'url' => '/',
+                        'controller' => 'App\Http\Controllers\HomeController@index',
+                        'name' => 'home',
+                        'middleware' => '',
+                    ],
+                    [
+                        'type' => 'public',
+                        'method' => 'get',
+                        'url' => '/products',
+                        'controller' => 'App\Http\Controllers\ProductController@index',
+                        'name' => 'products',
+                        'middleware' => '',
+                    ]
+                ]
+            ],
+            [
+                'title' => 'Document Upload Routes',
+                'item' => [
+                    [
+                        'type' => 'documents',
+                        'method' => 'get',
+                        'url' => '/documents/upload/repair',
+                        'controller' => 'App\Http\Controllers\DocumentUploadController@repairForm',
+                        'name' => 'documents.upload.repair',
+                        'middleware' => '',
+                    ],
+                    [
+                        'type' => 'documents',
+                        'method' => 'get',
+                        'url' => '/documents/upload/format',
+                        'controller' => 'App\Http\Controllers\DocumentUploadController@formatForm',
+                        'name' => 'documents.upload.format',
+                        'middleware' => '',
+                    ],
+                    [
+                        'type' => 'documents',
+                        'method' => 'get',
+                        'url' => '/documents/upload/plagiarism',
+                        'controller' => 'App\Http\Controllers\DocumentUploadController@plagiarismForm',
+                        'name' => 'documents.upload.plagiarism',
+                        'middleware' => '',
+                    ],
+                    [
+                        'type' => 'documents',
+                        'method' => 'post',
+                        'url' => '/documents/upload/repair',
+                        'controller' => 'App\Http\Controllers\DocumentUploadController@repairSubmit',
+                        'name' => 'documents.upload.repair.submit',
+                        'middleware' => '',
+                    ],
+                    [
+                        'type' => 'documents',
+                        'method' => 'post',
+                        'url' => '/documents/upload/format',
+                        'controller' => 'App\Http\Controllers\DocumentUploadController@formatSubmit',
+                        'name' => 'documents.upload.format.submit',
+                        'middleware' => '',
+                    ],
+                    [
+                        'type' => 'documents',
+                        'method' => 'post',
+                        'url' => '/documents/upload/plagiarism',
+                        'controller' => 'App\Http\Controllers\DocumentUploadController@plagiarismSubmit',
+                        'name' => 'documents.upload.plagiarism.submit',
+                        'middleware' => '',
+                    ]
+                ]
+            ],
+            [
+                'title' => 'Payment Routes',
+                'item' => [
+                    [
+                        'type' => 'payment',
+                        'method' => 'get',
+                        'url' => '/payment/{orderNumber}',
+                        'controller' => 'App\Http\Controllers\PaymentController@show',
+                        'name' => 'payment.show',
+                        'middleware' => '',
+                    ],
+                    [
+                        'type' => 'payment',
+                        'method' => 'post',
+                        'url' => '/payment/{orderNumber}/confirm',
+                        'controller' => 'App\Http\Controllers\PaymentController@confirm',
+                        'name' => 'payment.confirm',
+                        'middleware' => '',
+                    ]
+                ]
+            ]
+        ];
+
+        if (!empty($index)) {
+            return !empty($data[$index]) ? $data[$index] : null;
+        }
+        return $data;
+    }
+
+    function getDataAdmin($index = null)
+    {
+        $data = [
+            [
+                'title' => 'Admin Authentication',
+                'item' => [
+                    [
+                        'type' => 'auth',
+                        'method' => 'get',
+                        'url' => '/admin/login',
+                        'controller' => 'App\Http\Controllers\Admin\AdminLoginController@showLoginForm',
+                        'name' => 'admin.login',
+                        'middleware' => 'admin.guest',
+                    ],
+                    [
+                        'type' => 'auth',
+                        'method' => 'post',
+                        'url' => '/admin/login',
+                        'controller' => 'App\Http\Controllers\Admin\AdminLoginController@login',
+                        'name' => 'admin.login.submit',
+                        'middleware' => 'admin.guest',
+                    ],
+                    [
+                        'type' => 'auth',
+                        'method' => 'post',
+                        'url' => '/admin/logout',
+                        'controller' => 'App\Http\Controllers\Admin\AdminLoginController@logout',
+                        'name' => 'admin.logout',
+                        'middleware' => 'admin',
+                    ]
+                ]
+            ],
+            [
+                'title' => 'Admin Dashboard',
+                'item' => [
+                    [
+                        'type' => 'dashboard',
+                        'method' => 'get',
+                        'url' => '/admin/dashboard',
+                        'controller' => 'App\Http\Controllers\Admin\DashboardController@index',
+                        'name' => 'admin.dashboard',
+                        'middleware' => 'admin',
+                    ],
+                    [
+                        'type' => 'dashboard',
+                        'method' => 'get',
+                        'url' => '/admin/dashboard/stats',
+                        'controller' => 'App\Http\Controllers\Admin\DashboardController@getStats',
+                        'name' => 'admin.dashboard.stats',
+                        'middleware' => 'admin',
+                    ],
+                    [
+                        'type' => 'dashboard',
+                        'method' => 'get',
+                        'url' => '/admin/dashboard/chart',
+                        'controller' => 'App\Http\Controllers\Admin\DashboardController@getChartData',
+                        'name' => 'admin.dashboard.chart',
+                        'middleware' => 'admin',
+                    ]
+                ]
+            ],
+            [
+                'title' => 'Product Management',
+                'item' => [
+                    [
+                        'type' => 'products',
+                        'method' => 'get',
+                        'url' => '/admin/products',
+                        'controller' => 'App\Http\Controllers\Admin\ProductController@index',
+                        'name' => 'admin.products.index',
+                        'middleware' => 'admin',
+                    ],
+                    [
+                        'type' => 'products',
+                        'method' => 'get',
+                        'url' => '/admin/products/create',
+                        'controller' => 'App\Http\Controllers\Admin\ProductController@create',
+                        'name' => 'admin.products.create',
+                        'middleware' => 'admin',
+                    ],
+                    [
+                        'type' => 'products',
+                        'method' => 'post',
+                        'url' => '/admin/products',
+                        'controller' => 'App\Http\Controllers\Admin\ProductController@store',
+                        'name' => 'admin.products.store',
+                        'middleware' => 'admin',
+                    ],
+                    [
+                        'type' => 'products',
+                        'method' => 'get',
+                        'url' => '/admin/products/{id}',
+                        'controller' => 'App\Http\Controllers\Admin\ProductController@show',
+                        'name' => 'admin.products.show',
+                        'middleware' => 'admin',
+                    ],
+                    [
+                        'type' => 'products',
+                        'method' => 'get',
+                        'url' => '/admin/products/{id}/edit',
+                        'controller' => 'App\Http\Controllers\Admin\ProductController@edit',
+                        'name' => 'admin.products.edit',
+                        'middleware' => 'admin',
+                    ],
+                    [
+                        'type' => 'products',
+                        'method' => 'put',
+                        'url' => '/admin/products/{id}',
+                        'controller' => 'App\Http\Controllers\Admin\ProductController@update',
+                        'name' => 'admin.products.update',
+                        'middleware' => 'admin',
+                    ],
+                    [
+                        'type' => 'products',
+                        'method' => 'delete',
+                        'url' => '/admin/products/{id}',
+                        'controller' => 'App\Http\Controllers\Admin\ProductController@destroy',
+                        'name' => 'admin.products.destroy',
+                        'middleware' => 'admin',
+                    ],
+                    [
+                        'type' => 'products',
+                        'method' => 'post',
+                        'url' => '/admin/products/toggle-visibility',
+                        'controller' => 'App\Http\Controllers\Admin\ProductController@toggleVisibility',
+                        'name' => 'admin.products.toggle.visibility',
+                        'middleware' => 'admin',
+                    ]
+                ]
+            ],
+            [
+                'title' => 'Orders Management',
+                'item' => [
+                    [
+                        'type' => 'orders',
+                        'method' => 'get',
+                        'url' => '/admin/orders',
+                        'controller' => 'App\Http\Controllers\Admin\OrderController@index',
+                        'name' => 'admin.orders.index',
+                        'middleware' => 'admin',
+                    ],
+                    [
+                        'type' => 'orders',
+                        'method' => 'get',
+                        'url' => '/admin/orders/filter',
+                        'controller' => 'App\Http\Controllers\Admin\OrderController@filter',
+                        'name' => 'admin.orders.filter',
+                        'middleware' => 'admin',
+                    ],
+                    [
+                        'type' => 'orders',
+                        'method' => 'get',
+                        'url' => '/admin/orders/create',
+                        'controller' => 'App\Http\Controllers\Admin\OrderController@create',
+                        'name' => 'admin.orders.create',
+                        'middleware' => 'admin',
+                    ],
+                    [
+                        'type' => 'orders',
+                        'method' => 'post',
+                        'url' => '/admin/orders',
+                        'controller' => 'App\Http\Controllers\Admin\OrderController@store',
+                        'name' => 'admin.orders.store',
+                        'middleware' => 'admin',
+                    ],
+                    [
+                        'type' => 'orders',
+                        'method' => 'get',
+                        'url' => '/admin/orders/{id}',
+                        'controller' => 'App\Http\Controllers\Admin\OrderController@show',
+                        'name' => 'admin.orders.show',
+                        'middleware' => 'admin',
+                    ],
+                    [
+                        'type' => 'orders',
+                        'method' => 'get',
+                        'url' => '/admin/orders/{id}/edit',
+                        'controller' => 'App\Http\Controllers\Admin\OrderController@edit',
+                        'name' => 'admin.orders.edit',
+                        'middleware' => 'admin',
+                    ],
+                    [
+                        'type' => 'orders',
+                        'method' => 'put',
+                        'url' => '/admin/orders/{id}',
+                        'controller' => 'App\Http\Controllers\Admin\OrderController@update',
+                        'name' => 'admin.orders.update',
+                        'middleware' => 'admin',
+                    ],
+                    [
+                        'type' => 'orders',
+                        'method' => 'delete',
+                        'url' => '/admin/orders/{id}',
+                        'controller' => 'App\Http\Controllers\Admin\OrderController@destroy',
+                        'name' => 'admin.orders.destroy',
+                        'middleware' => 'admin',
+                    ]
+                ]
+            ],
+            [
+                'title' => 'Settings Management',
+                'item' => [
+                    [
+                        'type' => 'settings',
+                        'method' => 'get',
+                        'url' => '/admin/settings',
+                        'controller' => 'App\Http\Controllers\Admin\SettingController@index',
+                        'name' => 'admin.settings.index',
+                        'middleware' => 'admin',
+                    ],
+                    [
+                        'type' => 'settings',
+                        'method' => 'post',
+                        'url' => '/admin/settings/business',
+                        'controller' => 'App\Http\Controllers\Admin\SettingController@updateBusiness',
+                        'name' => 'admin.settings.business',
+                        'middleware' => 'admin',
+                    ],
+                    [
+                        'type' => 'settings',
+                        'method' => 'put',
+                        'url' => '/admin/settings/profile',
+                        'controller' => 'App\Http\Controllers\Admin\SettingController@updateProfile',
+                        'name' => 'admin.settings.profile.update',
+                        'middleware' => 'admin',
+                    ],
+                    [
+                        'type' => 'settings',
+                        'method' => 'post',
+                        'url' => '/admin/settings/password',
+                        'controller' => 'App\Http\Controllers\Admin\SettingController@updatePassword',
+                        'name' => 'admin.settings.password',
+                        'middleware' => 'admin',
+                    ]
+                ]
+            ]
+        ];
+
+        if (!empty($index)) {
+            return !empty($data[$index]) ? $data[$index] : null;
+        }
+        return $data;
+    }
+
+    function getIgnoreType($type = null)
+    {
+        $data = ['/', 'form', 'ajax', 'system'];
+        if (!empty($type)) {
+            if (!in_array($type, $data)) {
+                return 1;
+            }
+            return 0;
+        }
+        return $data;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Session;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\DocumentUploadController;
-use App\Http\Controllers\PaymentController;
-use App\Http\Controllers\Admin\AdminLoginController;
-use App\Http\Controllers\Admin\DashboardController;
-use App\Http\Controllers\Admin\OrderController;
-use App\Http\Controllers\Admin\SettingController;
+use App\Classes\ListRoutes;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,137 +14,109 @@ use App\Http\Controllers\Admin\SettingController;
 |
 */
 
-// Public Routes
-Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/products', [ProductController::class, 'index'])->name('products');
+// Public Routes (No Middleware)
+$publicRoutes = (new ListRoutes)->getDataAuth();
+foreach ($publicRoutes as $key_list => $value_list) {
+    if (!empty($value_list['item'])) {
+        $item = $value_list['item'];
 
-// Redirect default login ke admin login (karena hanya ada admin login)
+        foreach ($item as $key => $value) {
+            $value = (object)$value;
+            if (!empty($value->method)) {
+                $hasil = null;
+
+                if (is_array($value->method)) {
+                    $hasil = Route::match($value->method, $value->url, $value->controller);
+                } else {
+                    if (strtoupper($value->method) == 'GET') {
+                        $hasil = Route::get($value->url, $value->controller);
+                    }
+                    if (strtoupper($value->method) == 'POST') {
+                        $hasil = Route::post($value->url, $value->controller);
+                    }
+                    if (strtoupper($value->method) == 'PUT') {
+                        $hasil = Route::put($value->url, $value->controller);
+                    }
+                    if (strtoupper($value->method) == 'DELETE') {
+                        $hasil = Route::delete($value->url, $value->controller);
+                    }
+                    if (strtoupper($value->method) == 'PATCH') {
+                        $hasil = Route::patch($value->url, $value->controller);
+                    }
+                }
+
+                if (!empty($hasil)) {
+                    if (!empty($value->name)) {
+                        $hasil->name($value->name);
+                    }
+
+                    if (!empty($value->middleware)) {
+                        $hasil->middleware($value->middleware);
+                    }
+                }
+            }
+        }
+    }
+}
+
+// Admin Routes (With Middleware)
+$adminRoutes = (new ListRoutes)->getDataAdmin();
+foreach ($adminRoutes as $key_list => $value_list) {
+    if (!empty($value_list['item'])) {
+        $item = $value_list['item'];
+
+        foreach ($item as $key => $value) {
+            $value = (object)$value;
+            if (!empty($value->method)) {
+                $hasil = null;
+
+                if (is_array($value->method)) {
+                    $hasil = Route::match($value->method, $value->url, $value->controller);
+                } else {
+                    if (strtoupper($value->method) == 'GET') {
+                        $hasil = Route::get($value->url, $value->controller);
+                    }
+                    if (strtoupper($value->method) == 'POST') {
+                        $hasil = Route::post($value->url, $value->controller);
+                    }
+                    if (strtoupper($value->method) == 'PUT') {
+                        $hasil = Route::put($value->url, $value->controller);
+                    }
+                    if (strtoupper($value->method) == 'DELETE') {
+                        $hasil = Route::delete($value->url, $value->controller);
+                    }
+                    if (strtoupper($value->method) == 'PATCH') {
+                        $hasil = Route::patch($value->url, $value->controller);
+                    }
+                }
+
+                if (!empty($hasil)) {
+                    if (!empty($value->name)) {
+                        $hasil->name($value->name);
+                    }
+
+                    if (!empty($value->middleware)) {
+                        $hasil->middleware($value->middleware);
+                    }
+                }
+            }
+        }
+    }
+}
+
+// Additional Routes
 Route::get('/login', function () {
     return redirect()->route('admin.login');
 })->name('login');
 
-// Document Upload Routes
-Route::prefix('documents')->name('documents.')->group(function () {
-    Route::get('/upload/repair', [DocumentUploadController::class, 'repairForm'])->name('upload.repair');
-    Route::get('/upload/format', [DocumentUploadController::class, 'formatForm'])->name('upload.format');
-    Route::get('/upload/plagiarism', [DocumentUploadController::class, 'plagiarismForm'])->name('upload.plagiarism');
-
-    Route::post('/upload/repair', [DocumentUploadController::class, 'repairSubmit'])->name('upload.repair.submit');
-    Route::post('/upload/format', [DocumentUploadController::class, 'formatSubmit'])->name('upload.format.submit');
-    Route::post('/upload/plagiarism', [DocumentUploadController::class, 'plagiarismSubmit'])->name('upload.plagiarism.submit');
+Route::get('/admin', function () {
+    if (\Session::get('admin_logged_in') && \Session::get('admin_id')) {
+        return redirect()->route('admin.dashboard');
+    }
+    return redirect()->route('admin.login');
 });
-
-// Payment Routes
-Route::prefix('payment')->name('payment.')->group(function () {
-    Route::get('/{orderNumber}', [PaymentController::class, 'show'])->name('show');
-    Route::post('/{orderNumber}/confirm', [PaymentController::class, 'confirm'])->name('confirm');
-});
-
-// Admin Authentication Routes
-Route::prefix('admin')->name('admin.')->group(function () {
-
-    // Ganti semua AdminLoginController dengan namespace baru:
-    Route::get('/login', [AdminLoginController::class, 'showLoginForm'])->name('login');
-    Route::post('/login', [AdminLoginController::class, 'login'])->name('login.submit');
-    Route::post('/logout', [AdminLoginController::class, 'logout'])->name('logout');
-
-    // Redirect /admin to dashboard or login
-    Route::get('/', function () {
-        if (Session::get('admin_logged_in') && Session::get('admin_id')) {
-            return redirect()->route('admin.dashboard');
-        }
-        return redirect()->route('admin.login');
-    });
-
-    // Protected Admin Routes (Admin Only)
-    Route::middleware('admin')->group(function () {
-
-        // Dashboard
-        Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-        Route::get('/dashboard/stats', [DashboardController::class, 'getStats'])->name('dashboard.stats');
-        Route::get('/dashboard/chart', [DashboardController::class, 'getChartData'])->name('dashboard.chart');
-        Route::get('/', function () {
-            if (Session::get('admin_logged_in') && Session::get('admin_id')) {
-                return redirect()->route('admin.dashboard');
-            }
-            return redirect()->route('admin.login');
-        });
-
-        // Product Management
-        Route::prefix('products')->name('products.')->group(function () {
-            Route::get('/', [\App\Http\Controllers\Admin\ProductController::class, 'index'])->name('index');
-            Route::get('/create', [\App\Http\Controllers\Admin\ProductController::class, 'create'])->name('create');
-            Route::post('/', [\App\Http\Controllers\Admin\ProductController::class, 'store'])->name('store');
-            Route::get('/{id}', [\App\Http\Controllers\Admin\ProductController::class, 'show'])->name('show');
-            Route::get('/{id}/edit', [\App\Http\Controllers\Admin\ProductController::class, 'edit'])->name('edit');
-            Route::put('/{id}', [\App\Http\Controllers\Admin\ProductController::class, 'update'])->name('update');
-            Route::delete('/{id}', [\App\Http\Controllers\Admin\ProductController::class, 'destroy'])->name('destroy');
-            Route::post('/toggle-visibility', [\App\Http\Controllers\Admin\ProductController::class, 'toggleVisibility'])->name('toggle.visibility');
-        });
-
-        // Orders Management
-        Route::prefix('orders')->name('orders.')->group(function () {
-            Route::get('/', [OrderController::class, 'index'])->name('index');
-            Route::get('filter', [OrderController::class, 'filter'])->name('filter');
-            Route::get('create', [OrderController::class, 'create'])->name('create');
-            Route::post('/', [OrderController::class, 'store'])->name('store');
-            Route::get('{id}', [OrderController::class, 'show'])->name('show');
-            Route::get('{id}/edit', [OrderController::class, 'edit'])->name('edit');
-            Route::put('{id}', [OrderController::class, 'update'])->name('update');
-            Route::delete('{id}', [OrderController::class, 'destroy'])->name('destroy');
-
-            // AJAX Routes
-            Route::get('{id}/details', [OrderController::class, 'getOrderDetails'])->name('details');
-            Route::post('{id}/update-payment-status', [OrderController::class, 'updatePaymentStatus'])->name('update-payment-status');
-            Route::get('{id}/download/{type}', [OrderController::class, 'downloadFile'])->name('download-file');
-            Route::get('export', [OrderController::class, 'export'])->name('export');
-        });
-
-        // Settings Management
-        Route::prefix('settings')->name('settings.')->group(function () {
-            Route::get('/', [SettingController::class, 'index'])->name('index');
-            Route::post('business', [SettingController::class, 'updateBusiness'])->name('business');
-            Route::post('profile', [SettingController::class, 'updateProfile'])->name('profile');
-            Route::put('/profile', [SettingController::class, 'updateProfile'])->name('profile.update');
-            Route::post('password', [SettingController::class, 'updatePassword'])->name('password');
-        });
-    });
-});
-
-// Storage access route for Vercel (karena tidak ada symlink)
-// Route::get('/storage/{path}', function ($path) {
-//     $filePath = storage_path('app/public/' . $path);
-
-//     if (!file_exists($filePath)) {
-//         abort(404);
-//     }
-
-//     $mimeType = mime_content_type($filePath);
-
-//     return response()->file($filePath, [
-//         'Content-Type' => $mimeType,
-//         'Cache-Control' => 'public, max-age=31536000',
-//     ]);
-// })->where('path', '.*');
 
 // Fallback route for 404
 Route::fallback(function () {
     return redirect()->route('home');
-});
-
-
-// Tambahkan temporary di web.php
-Route::get('/debug-files', function () {
-    $files = [
-        'AdminLoginController' => file_exists(app_path('Http/Controllers/Admin/AdminLoginController.php')),
-        'DashboardController' => file_exists(app_path('Http/Controllers/Admin/DashboardController.php')),
-        'SettingController' => file_exists(app_path('Http/Controllers/Admin/SettingController.php')),
-    ];
-
-    return response()->json([
-        'app_path' => app_path(),
-        'files' => $files,
-        'directory_contents' => is_dir(app_path('Http/Controllers/Admin')) ?
-            scandir(app_path('Http/Controllers/Admin')) : 'Directory not found'
-    ]);
 });
